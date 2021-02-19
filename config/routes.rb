4232b6467b543a43898a5b064a08e6cfb59c105f
Antoine Boucher <antoine.boucher@mail.mcgill.ca>
2021-02-18 20:35:46 -0500

    challenge #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # verb 'path', to: 'controller#action'
+
+
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+
+
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
